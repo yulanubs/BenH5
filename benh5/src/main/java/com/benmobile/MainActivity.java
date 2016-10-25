17fe882bc624fd9bfce58b99dee0fafa1f59752c
Jekshow <yulanubs123@qq.com>
2016-10-25 17:17:05 +0800

    参考appcan,添加了主页

---
+++ benh5/src/main/java/com/benmobile/MainActivity.java
@@ -0,0 +1,16 @@
+package com.benmobile;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+import com.benh5.R;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
