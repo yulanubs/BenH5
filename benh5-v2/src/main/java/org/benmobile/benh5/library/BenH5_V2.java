429a89632916f822532f508b15be008f58e0b69b
Jekshow <yulanubs123@qq.com>
2017-02-18 19:01:37 +0800

    添加了bentools库

--- benh5-v2/src/main/java/org/benmobile/benh5/library/BenH5_V2.java
+++ benh5-v2/src/main/java/org/benmobile/benh5/library/BenH5_V2.java
@@ -1,12 +1,14 @@
 package org.benmobile.benh5.library;
-
+import org.bentools.utils.date.DateUtil;
 /**
  * Created by Jekshow on 2017/2/14.
  */
-
 public class BenH5_V2 {
     private static BenH5_V2 instance;
 
+    /**
+     * @return  BenH5_V2 Instan
+     */
     public static BenH5_V2 getInstance() {
         if (instance == null)
             synchronized (BenH5_V2.class) {
@@ -15,7 +17,14 @@ public class BenH5_V2 {
             }
         return instance;
     }
-    private  String  getBenH5Info(String name){
-        return "BenH5_V2"+"欢迎您："+name;
+
+    /**
+     *
+     * @param name
+     * @return BenH5Info
+     */
+    private String getBenH5Info(String name) {
+        String currentDate = DateUtil.getCurrentDate();
+        return name + "_BenH5_V2_" + currentDate;
     }
 }
