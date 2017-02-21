4b6c4651266238021ecb9f104975d4f7538ef9e2
Jekshow <yulanubs123@qq.com>
2017-02-21 13:08:59 +0800

    恢复首页

---
+++ app/src/main/java/org/benmobile/LoadingActivity.java
@@ -0,0 +1,32 @@
+package org.benmobile;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+import org.benmobile.R;
+
+/**
+ *LoadingActivity
+ * 启动Activity
+ */
+public class LoadingActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_loading);
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Thread.sleep(1000);
+                    LoadingActivity.this.startActivity(new Intent(LoadingActivity.this,MainActivity.class));
+                    LoadingActivity.this.finish();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+    }
+}
---
+++ app/src/main/java/org/benmobile/MainActivity.java
@@ -0,0 +1,16 @@
+package org.benmobile;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+import org.benmobile.R;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
